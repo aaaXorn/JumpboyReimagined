1be0d63358d2e89fcb5f0b898bd0edb93e66da0c
Xorn <lcrs.rp@gmail.com>
2022-03-29 15:57:02 -0300

    esboço do código do pulo e do slide

--- Assets/Scripts/JumpboyControl.cs
+++ Assets/Scripts/JumpboyControl.cs
@@ -4,29 +4,77 @@ using UnityEngine;
 
 public class JumpboyControl : MonoBehaviour
 {
-	bool landed;//se o jogador está no chão
+	Rigidbody rigid;
 	
+	bool landed = true;//se o jogador está no chão
+	
+	#region jump
 	bool jump_holding;//se o player está carregando um pulo
 	float jump_charge, max_jump_charge;//carga do pulo atual e máxima
 	
-	Rigidbody rigid;
+	float jump_base_force, jump_charge_force;//força base e por carregar do pulo
+	#endregion
 	
-    // Start is called before the first frame update
+	#region slide
+	bool sliding;//se o jogador está dando um slide
+	
+	[SerializeField]
+	Collider head_col;//collider da cabeça, desabilitado durante o slide
+	#endregion
+	
+    //inicializa variáveis
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 	
-	//update pra física
+	//física do jogo
 	void FixedUpdate()
 	{
+		//carregando o pulo
+		//fora do if(landed) pra servir como buffer
+		if(jump_holding)
+		{
+			//timer
+			jump_charge += Time.deltaTime;
+			
+			//se o timer passar do máximo
+			if(jump_charge >= max_jump_charge)
+			{
+				jump_charge = max_jump_charge;
+				jump_holding = false;
+			}
+		}
 		
+		//se o jogador está no chão
+		if(landed)
+		{
+			//slide
+			if(sliding)
+			{
+				//tira a hurtbox da cabeça
+				if(head_col.enabled) head_col.enabled = true;
+				
+				
+			}
+			//idle e pulo
+			else
+			{
+				//retorna a hurtbox da cabeça
+				if(!head_col.enabled) head_col.enabled = false;
+				
+				//pulo
+				if(jump_charge > 0)
+				{
+					//força do pulo
+					float forceY = jump_base_force + (jump_charge_force * jump_charge / max_jump_charge);
+					rigid.AddForce(new Vector3(0, forceY, 0), ForceMode.Impulse);
+					
+					//reseta o charge do pulo
+					jump_charge = 0;
+				}
+			}
+		}
 	}
 	
 	#region buttons
@@ -34,13 +82,25 @@ public class JumpboyControl : MonoBehaviour
 	public void JumpButtonPress()
 	{
 		jump_holding = true;
-		jump_charge = 0;
 	}
-	
 	//botão de pulo soltado
 	public void JumpButtonRelease()
 	{
+		//sobrepõe o slide
+		sliding = false;
+		
 		jump_holding = false;
+	}
+	
+	//botão de slide apertado
+	public void SlideButtonPress()
+	{
+		sliding = true;
+	}
+	//botão de slide soltado
+	public void SlideButtonRelease()
+	{
+		sliding = false;
 	}
 	#endregion
 }
